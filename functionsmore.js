216b52f8565b329b80bf151fa21e2624c1b1bcab
Chris <minaoanya@gmail.com>
2023-05-25 16:35:27 +0300

    More on functions, declaration ways

---
+++ functionsmore.js
@@ -0,0 +1,16 @@
+/*DEFINING A FUNCTION USING FUNCTION DECLARATION*/
+/*The most common way of to define a function is by use of a function declaration.*/
+
+function myName(){
+    console.log("Miriam");
+}
+
+/*"myName" is a pointer to some stored, potential, not yet actually run bit of work
+(the function). The pointer is used to call, invoke or execute the function. We call/invoke 
+a function by adding () after the pointer"*/
+
+
+function myName(){
+    console.log("Miriam")
+}
+myName();
