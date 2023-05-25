79e140079c9dd7738d6044cb911e39d023d49514
Chris <minaoanya@gmail.com>
2023-05-25 16:07:38 +0300

    Calculated fare for the customer

--- functionslab.js
+++ functionslab.js
@@ -53,3 +53,31 @@ function distanceTravelledInFeet(start, destination){
 
 const myDistance = distanceTravelledInFeet(34, 38);
 console.log(myDistance);
+
+/*Given the same starting and ending block as the above test, return the fare for the customer.
+The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2
+cents per foot(not including 400, which are free!). Then scuber charges a flat fare for a distance
+over 2000 feet and under 25000 feet. Finally, Scuber does not allow any rides over 2500 feet - the 
+functions returns 'cannot travel that far' if a ride over 2500 feet is requested.*/
+
+function calculateFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if(distance <= 400){
+        return 0;
+    }
+
+    else if(distance >= 400 && distance <= 2000){
+        return (distance-400)* 0.02;
+    }
+
+    else if(distance >= 2000 && distance <= 2500){
+        return 25;
+    }
+
+    else if(distance > 2500){
+        return "cannot travel that far"
+    }
+}
+
+const lastOne = calculateFarePrice(30, 80);
+console.log(lastOne)
